e8b01de91ae66e1dfa87fc15002d399403a2fa29
Satya753 <satyajeet753@gmail.com>
2023-06-03 22:25:55 +0530

    Initialization

---
+++ spingo-frontend/src/Components/EmailAdd.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './EmailAdd.css'
+function EmailAdd(){
+
+    return <div className="EmailHeading">
+        <div className="block">
+        <h1 className="heading content">Get your daily dose of organic juice with JuiceGo</h1>
+
+        <h3 className="content">JuiceGo is here to make it easy for you to get your daily dose of organic, natural and healthy juice right at your doorstep.</h3>
+        </div>
+        <div className="block">
+        </div>
+    </div>
+}
+
+export default EmailAdd;
